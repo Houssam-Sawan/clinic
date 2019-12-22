157d1e08e3f78b383976dabe0bbda3c02db98d61
houssam-sawan <eng.houssam.sawan@gmail.com>
2019-12-22 08:25:27 +0300

    create simple index page

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    $title = 'Home';
+    include 'head.php';
+
+?>
+
+<body>
+<div class="jumbotron">
+<button type="button" class="btn btn-primary">Hello</button>
+
+</div>
+</body>
+</html>
