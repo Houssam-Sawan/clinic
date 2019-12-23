d2970edc4d67d328acf078bdff0275c3ea41cebf
houssam-sawan <eng.houssam.sawan@gmail.com>
2019-12-23 07:59:46 +0300

    add jquery library

--- head.php
+++ head.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 
     <title><?=$title?></title>
 </head>
