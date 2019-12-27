0d082b9638161ecc0fbee8538e8945debff90783
houssam-sawan <eng.houssam.sawan@gmail.com>
2019-12-27 20:58:29 +0300

    add custom style

--- head.php
+++ head.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
     <script src="js/jquery-3.4.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 
