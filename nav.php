541008cf8131ba10f4ce4f8bdf0aea5ab19cd50c
houssam-sawan <eng.houssam.sawan@gmail.com>
2019-12-27 22:14:36 +0300

    navbar adjustment

--- nav.php
+++ nav.php
@@ -1,58 +1,46 @@
     <nav class="navbar navbar-default">
-        <div class="container">
+        <div class="container-fluid">
             <div class="navbar-header">
-                <a class="navbar-brand" href="index.php">PHARMA</a>
+                <a class="navbar-brand" href="index.php">CLINIC</a>
             </div>
             <ul class="nav navbar-nav">
                 <li class="active">
                     <a href="index.php">Home</a>
                 </li>
             <?php
-            if(@$_SESSION['Username'] == 'admin' ){
+            if(@$_SESSION['Username'] == 'admin'){
                 echo '<li>
-                        <a href="viewproducts.php">View Products</a>
+                        <a href="addpatient.php">Add Patient</a>
                     </li>
                     <li>
-                        <a href="vieworders.php">View Orders</a>
+                        <a href="viewpatients.php">View Patient</a>
                     </li>
                     <li>
-                        <a href="register.php"> Add User</a>
+                        <a href="adddrugitem.php">Add Medicine</a>
                     </li>
                     <li>
-                        <a href="additem.php"> Add Items</a>
+                        <a href="addprescription.php">Add Prescription</a>
                     </li>
                     <li>
-                        <a href="addnews.php"> Publish Event</a>
+                        <a href="groups.php">Groups</a>
+                    </li>
+                    <li>
+                        <a href="addgroup.php">Add Group</a>
+                    </li>
+                    <li>
+                        <a href="users.php">Users</a>
+                    </li>
+                    <li>
+                        <a href="adduser.php">Add User</a>
                     </li>';
             }
             if(@$_SESSION['LoggedIn'] == 1 ){
-                echo '<li>
-                        <a href="submitinventory.php">Submit Invertory</a>
-                    </li>';
-                echo '<li>
-                        <a href="submitorder.php">Submit Order</a>
-                    </li>';
+                
             }
             ?>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-<!--
-                <li>
-                    <a href="register.php">
-                        <span class="glyphicon glyphicon-user"></span> Sign Up</a>
-                </li>
-                <li>
-                    <a href="index.php">
-                        <span class="glyphicon glyphicon-log-in"></span> Login</a>
-                </li>
-                <li>
-                    <a href="logout.php">
-                        <span class="glyphicon glyphicon-log-out"></span> Logout</a>
-                </li>
-                <li>
-                    <a href="#">testseparator</a>
-                </li>
--->
+
                 <?php
                 if(@$_SESSION['LoggedIn'] == 1 ){
                     echo '<li>
