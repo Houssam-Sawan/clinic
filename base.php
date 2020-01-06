55e18d1fdd3bb75a5a3596e5394553b7f73dd02d
houssam-sawan <eng.houssam.sawan@gmail.com>
2020-01-06 08:09:14 +0300

    database login details

--- base.php
+++ base.php
@@ -3,8 +3,8 @@ session_start();
 
 $dbhost = "localhost"; // this will ususally be 'localhost', but can sometimes differ
 $dbname = "clinic"; // the name of the database that you are going to use for this project
-$dbuser = "root"; // the username that you created, or were given, to access your database
-$dbpass = ""; // the password that you created, or were given, to access your database
+$dbuser = "clinicadmin"; // the username that you created, or were given, to access your database
+$dbpass = "clinicadmin"; // the password that you created, or were given, to access your database
 
 $dbc = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)
 OR die('Could not connect to database' .
