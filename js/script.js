6dc58e1a73a4eadcabb3af1f5c63a4a5e01b7528
houssam-sawan <eng.houssam.sawan@gmail.com>
2020-01-26 17:55:22 +0300

    add script file

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    var all = document.getElementsByTagName("*");
+
+    for (var i=0, max=all.length; i < max; i++) {
+         // Do something with the element here
+         //all[i].style.backgroundColor = "#fff";
+    }
+    
+});
+
+
