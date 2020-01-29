af46829ca96ba77d920fe89a67e6e9f4e8095870
houssam-sawan <eng.houssam.sawan@gmail.com>
2020-01-29 06:07:29 +0300

    implement add drugs

--- adddrugitem.php
+++ adddrugitem.php
@@ -1,17 +1,77 @@
 <?php
 $title = 'New Medicin';
 include 'head.php';
-if($_SESSION['LoggedIn'] != 1 || $_SESSION['prev'] != "ADMIN"){
-    echo '<script language="javascript" type="text/javascript">
-    location.href = "index.php";
-    </script>
-    ';
-}
+    if($_SESSION['LoggedIn'] != 1 || $_SESSION['prev'] != "ADMIN"){
+        echo '<script language="javascript" type="text/javascript">
+        location.href = "index.php";
+        </script>
+        ';
+    }
  ?>
  <body>
  <?php
   include 'nav.php';
 
+  if(!empty($_POST['medicin']))
+  {
+      $medName = trim($_POST['medicin']);
+      $medType = trim(@$_POST['type']);
+      $medDose = trim(@$_POST['dose']);
+      $notes = trim(@$_POST['notes']);
+  
+      $sql =<<<EOF
+          SELECT * FROM drugs WHERE drug_name = '$medName';
+EOF;
+  
+      $db = new MyDB();
+      if(!$db) {
+      echo $db->lastErrorMsg();
+      } 
+  
+      $result = $db->query($sql);
+      if($result){
+          while( $row = $result->fetchArray(SQLITE3_ASSOC)){
+              if($row['drug_name'] == $medName){
+              error('This medicin already exists.\\n'.
+                  'Please try another.');
+              }
+          }
+  
+      }else {
+          error('A database error occurred in processing your '.
+          'submission.\\nIf this error persists, please '.
+          'contact Support.');
+      }
+      $db->close();
+      unset($db);
+  
+      $query =<<<EOF
+      INSERT INTO drugs(drug_name, type, dose, notes) 
+                    VALUES('$medName', '$medType','$medDose', '$notes');
+EOF;
+  
+      $db = new MyDB();
+          if(!$db) {
+          echo $db->lastErrorMsg();
+          } 
+      $result = $db->exec($query);
+      if(!$result) {
+      echo $db->lastErrorMsg();
+      } else {
+          echo '<div class="container">
+          <div class="row">
+              <div class="col-sm-6 col-md-4 col-md-offset-4 col-sm-offset-3">
+                  <h1 class="text-center login-title main-title">Medicin added successfuly</h1>
+                  <h1 class="text-center login-title">To add another Medicin please click
+                  <a href="adddrugitem.php">here</a></h1>
+          </div></div></div>';
+      }
+      $db->close();
+      unset($db);
+  
+  }
+  else
+  {
  ?>
 
  <div class="container">
@@ -33,3 +93,14 @@ if($_SESSION['LoggedIn'] != 1 || $_SESSION['prev'] != "ADMIN"){
          </div>
      </div>
  </div>
+ <?php
+}
+?>
+<datalist id="types">
+    <option value="Pill"></option>
+    <option value="IV"></option>
+    <option value="Other"></option>
+</datalist>
+</div>
+</body>
+</html>
